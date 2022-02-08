c9c80cb35ecd4cdcaf6435a567860c0eb3966b27
Jonathan Teran <jonathan.nerat@gmail.com>
2022-02-08 01:07:37 -0300

    update tabby ui

--- lua/my/plugins/tabby.lua
+++ lua/my/plugins/tabby.lua
@@ -1,18 +1,13 @@
 local M = {}
 
 M.setup = function()
-	local layout = require "tabby.presets".active_wins_at_tail
+	local layout = require "tabby.presets".tab_only
 	layout.head[1][1] =  '   '
-	layout.head[2][1] = ''
-	layout.active_tab.left_sep[1] = ''
-	layout.active_tab.right_sep[1] = ''
-	layout.inactive_tab.left_sep[1] = ''
-	layout.inactive_tab.right_sep[1] = ''
-	layout.top_win.left_sep[1] = ''
-	layout.top_win.right_sep[1] = ''
-	layout.win.left_sep[1] = ''
-	layout.win.right_sep[1] = ''
-	layout.tail[1][1] = ''
+	layout.head[2][1] = ''
+	layout.active_tab.left_sep[1] = ''
+	layout.active_tab.right_sep[1] = ''
+	layout.inactive_tab.left_sep[1] = ''
+	layout.inactive_tab.right_sep[1] = ''
 
 	require("tabby").setup {
 		tabline = layout,
