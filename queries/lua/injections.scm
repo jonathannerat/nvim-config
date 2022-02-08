c1c67e84bb18fa7dfde75c8a18d657d66fbc5c4d
Jonathan Teran <jonathan.nerat@gmail.com>
2022-02-08 01:11:31 -0300

    comment out injections because they don't work

--- queries/lua/injections.scm
+++ queries/lua/injections.scm
@@ -1,29 +1,29 @@
-((local_variable_declaration
-	(variable_declarator (identifier) @_identifier)
-	(table
-		(field (string) @html)
-	)
-	(#eq? @_identifier "html_snippets")
-))
-
-((local_variable_declaration
-	(variable_declarator (identifier) @_identifier)
-	(table
-		(field (string) @c)
-	)
-	(#eq? @_identifier "c_snippets")
-))
-
-((local_variable_declaration
-	(variable_declarator (identifier) @_identifier)
-	(table
-		(field (string) @latex)
-	)
-	(#eq? @_identifier "tex_snippets")
-))
-
-((local_variable_declaration
-	(variable_declarator (identifier) @_identifier)
-	(string) @vim
-	(#match? @_identifier "vim_cmds_(pre|post)")
-))
+;((local_variable_declaration
+;	(variable_declarator (identifier) @_identifier)
+;	(table
+;		(field (string) @html)
+;	)
+;	(#eq? @_identifier "html_snippets")
+;))
+;
+;((local_variable_declaration
+;	(variable_declarator (identifier) @_identifier)
+;	(table
+;		(field (string) @c)
+;	)
+;	(#eq? @_identifier "c_snippets")
+;))
+;
+;((local_variable_declaration
+;	(variable_declarator (identifier) @_identifier)
+;	(table
+;		(field (string) @latex)
+;	)
+;	(#eq? @_identifier "tex_snippets")
+;))
+;
+;((local_variable_declaration
+;	(variable_declarator (identifier) @_identifier)
+;	(string) @vim
+;	(#match? @_identifier "vim_cmds_(pre|post)")
+;))
