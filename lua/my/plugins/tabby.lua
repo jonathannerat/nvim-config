6c3465a0e5dbc577f98fa0633a0410828d14ba62
Jonathan Teran <jonathan.nerat@gmail.com>
2022-03-23 00:46:29 -0300

    tabby: use layout preset

--- lua/my/plugins/tabby.lua
+++ lua/my/plugins/tabby.lua
@@ -1,16 +1,8 @@
 local M = {}
 
 M.setup = function()
-	local layout = require "tabby.presets".tab_only
-	layout.head[1][1] =  '   '
-	layout.head[2][1] = ''
-	layout.active_tab.left_sep[1] = ''
-	layout.active_tab.right_sep[1] = ''
-	layout.inactive_tab.left_sep[1] = ''
-	layout.inactive_tab.right_sep[1] = ''
-
 	require("tabby").setup {
-		tabline = layout,
+		tabline = require("tabby.presets").tab_with_top_win,
 	}
 end
 
