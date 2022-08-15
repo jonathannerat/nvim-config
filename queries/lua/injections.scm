3356348222b34a5fe309ab0b1e2d1c170f9d7b24
Jonathan Teran <jonathan.nerat@gmail.com>
2022-08-15 15:45:58 -0300

    add injections for lua_command

--- queries/lua/injections.scm
+++ queries/lua/injections.scm
@@ -1,29 +1,4 @@
-;((local_variable_declaration
-;	(variable_declarator (identifier) @_identifier)
-;	(table
-;		(field (string) @html)
-;	)
-;	(#eq? @_identifier "html_snippets")
-;))
-;
-;((local_variable_declaration
-;	(variable_declarator (identifier) @_identifier)
-;	(table
-;		(field (string) @c)
-;	)
-;	(#eq? @_identifier "c_snippets")
-;))
-;
-;((local_variable_declaration
-;	(variable_declarator (identifier) @_identifier)
-;	(table
-;		(field (string) @latex)
-;	)
-;	(#eq? @_identifier "tex_snippets")
-;))
-;
-;((local_variable_declaration
-;	(variable_declarator (identifier) @_identifier)
-;	(string) @vim
-;	(#match? @_identifier "vim_cmds_(pre|post)")
-;))
+(
+    (function_call (identifier) @_identifier (arguments _ (string content: _ @lua)))
+    (#match? @_identifier "lua_command")
+)
