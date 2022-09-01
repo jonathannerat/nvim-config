3c56847f376ebcc268b15b8cf6e9b0857427e612
Jonathan Teran <jonathan.nerat@gmail.com>
2022-08-31 23:18:52 -0300

    lspconfig: fix lspsaga, expose less stuff

--- lua/my/plugins/lspconfig.lua
+++ lua/my/plugins/lspconfig.lua
@@ -5,9 +5,9 @@ local map = Mapper:new()
 
 local default_mappings = {
    ["<C-k>"] = luacmd "vim.lsp.buf.signature_help()",
-   ["gA"] = luacmd 'require("lspsaga.codeaction").code_action()',
-   ["gR"] = luacmd 'require("lspsaga.rename").lsp_rename()',
-   ["gh"] = luacmd 'require("lspsaga.finder").lsp_finder()',
+   ["gA"] = luacmd 'require("lspsaga.codeaction"):code_action()',
+   ["gR"] = luacmd 'require("lspsaga.rename"):lsp_rename()',
+   ["gh"] = luacmd 'require("lspsaga.finder"):lsp_finder()',
    ["K"] = luacmd "vim.lsp.buf.hover()",
    ["[D"] = luacmd 'require("lspsaga.diagnostic").show_line_diagnostics()',
    ["[d"] = luacmd 'require("lspsaga.diagnostic").goto_prev()',
@@ -18,9 +18,7 @@ local default_mappings = {
    ["gr"] = luacmd "vim.lsp.buf.references()",
 }
 
-local M = {}
-
-M.on_attach = function(client, bufnr)
+local function on_attach(client, bufnr)
    map:buffer(bufnr, function(m)
       m:with_options({ silent = true, noremap = true }, function(m)
          m:bind(default_mappings)
@@ -34,12 +32,11 @@ M.on_attach = function(client, bufnr)
          end
       end)
    end)
-
-   require("lspkind").init()
 end
 
-M.capabilities = vim.lsp.protocol.make_client_capabilities()
-vim.tbl_extend("force", M.capabilities.textDocument.completion.completionItem, {
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+
+vim.tbl_extend("force", capabilities.textDocument.completion.completionItem, {
    snippetSupport = true,
    preselectSupport = true,
    insertReplaceSupport = true,
@@ -56,36 +53,36 @@ vim.tbl_extend("force", M.capabilities.textDocument.completion.completionItem, {
    },
 })
 
-M.lsp_servers = {
-   clangd = {},
+local lsp_servers = {
+   ccls = {},
+   pylsp = {},
+   pyright = {},
+   tsserver = {},
+   volar = {},
+   sumneko_lua = require("lua-dev").setup(),
    intelephense = {
       init_options = {
          globalStoragePath = os.getenv "HOME" .. "/.local/share/intelephense",
          licenceKey = "EducationalCode",
       },
    },
-   pylsp = {},
-   pyright = {},
-   sumneko_lua = require("lua-dev").setup(),
-   tsserver = {},
-   volar = {},
 }
 
-M.setup = function()
-   require("nvim-lsp-installer").setup {}
+return {
+   setup = function()
+      require("nvim-lsp-installer").setup {}
 
-   local lspconfig = require "lspconfig"
+      local lspconfig = require "lspconfig"
 
-   for name, opts in pairs(M.lsp_servers) do
-      if not opts.on_attach then
-         opts.on_attach = M.on_attach
+      for name, opts in pairs(lsp_servers) do
+         if not opts.on_attach then
+            opts.on_attach = on_attach
+         end
+         if not opts.capabilities then
+            opts.capabilities = capabilities
+         end
+
+         lspconfig[name].setup(opts)
       end
-      if not opts.capabilities then
-         opts.capabilities = M.capabilities
-      end
-
-      lspconfig[name].setup(opts)
-   end
-end
-
-return M
+   end,
+}
