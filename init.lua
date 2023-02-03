f51d8a8baec5c3cdb460c5dc026376294b38b24c
Jonathan Teran <jonathan.nerat@gmail.com>
2023-02-03 16:12:43 -0300

    remove package.path modifications no longer needed

--- init.lua
+++ init.lua
@@ -1,13 +1,4 @@
-local function add_to_path(path)
-   if not string.match(";" .. package.path .. ";", ";" .. path .. ";") then
-      package.path = path .. ";" .. package.path
-   end
-end
-
 local custom = require "my.utils".custom
-
-add_to_path "./lua/?.lua"
-add_to_path "./lua/?/init.lua"
 
 require "my.options"
 require "my.plugins"
