326368c305a826bf786ff5f8009362c9607fa5f7
Jonathan Teran <jonathan.nerat@gmail.com>
2023-03-23 23:39:09 -0300

    neotree: update config

--- lua/my/plugins/neo-tree.lua
+++ lua/my/plugins/neo-tree.lua
@@ -1,9 +1,45 @@
 local neotree = require "neo-tree"
+local manager = require "neo-tree.sources.manager"
 
 local M = {
    config = {
-      enable_git_status = false,
-      enable_diagnostics = false,
+      close_if_last_window = true,
+      sources = {
+         "filesystem",
+         "buffers",
+         "git_status",
+      },
+      source_selector = {
+         winbar = true,
+         statusline = false, -- toggle to show selector on statusline
+         content_layout = "center",
+         tabs_layout = "equal",
+         tab_labels = {
+            filesystem = "" .. " Files",
+            buffers = "" .. " Bufs",
+            git_status = "" .. " Git",
+            diagnostics = "裂" .. " Diags",
+         },
+      },
+      default_component_configs = {
+         indent = {
+            indent_size = 2,
+            padding = 1, -- extra padding on left hand side
+
+            expander_collapsed = "",
+            expander_expanded = "",
+            expander_highlight = "NeoTreeExpander",
+         },
+         modified = {
+            symbol = "[+]",
+            highlight = "NeoTreeModified",
+         },
+         name = {
+            trailing_slash = false,
+            use_git_status_colors = true,
+            highlight = "NeoTreeFileName",
+         },
+      },
       window = {
          mappings = {
             ["<c-x>"] = "split_with_window_picker",
@@ -19,7 +55,7 @@ local M = {
          {
             event = "file_opened",
             handler = function()
-               neotree.close "filesystem"
+               manager.close_all()
             end,
          },
       },
