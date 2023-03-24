a48f5aedf1c011c9f6b0d29e8d7202c519b82577
Jonathan Teran <jonathan.nerat@gmail.com>
2023-03-23 23:34:01 -0300

    neorg: update modules config

--- lua/my/plugins.lua
+++ lua/my/plugins.lua
@@ -254,9 +254,14 @@ require("lazy").setup {
       opts = {
          load = {
             ["core.defaults"] = {},
+            ["core.keybinds"] = {
+               config = {
+                  default_keybinds = true,
+               },
+            },
             ["core.norg.concealer"] = {},
             ["core.norg.dirman"] = {
-               config = { workspaces = { notes = "~/notebook/home/" }, autochdir = true },
+               config = { workspaces = { notes = "~/notebook/" }, autochdir = true },
             },
             ["core.norg.completion"] = {
                config = { engine = "nvim-cmp" },
