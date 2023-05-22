dff33883ca8a8783c50cd387073b97c59093a01c
Jonathan Teran <jonathan.nerat@gmail.com>
2023-05-22 10:04:44 -0300

    telescope: remove version pinning at 0.1.1

--- lua/user/plugins/ui.lua
+++ lua/user/plugins/ui.lua
@@ -33,7 +33,6 @@ return {
    {
       "nvim-telescope/telescope.nvim",
       cmd = "Telescope",
-      tag = "0.1.1",
       dependencies = {
          "nvim-lua/plenary.nvim",
          "nvim-telescope/telescope-live-grep-args.nvim",
