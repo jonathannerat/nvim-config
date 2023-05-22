582886296e755e82f7b9775f90950b5efc835e5d
Jonathan Teran <jonathan.nerat@gmail.com>
2023-05-22 10:13:29 -0300

    vimtex: use tex and latex

--- lua/user/plugins/integrations.lua
+++ lua/user/plugins/integrations.lua
@@ -6,7 +6,7 @@ local silent = utils.silent
 return {
    { -- LaTeX integration
       "lervag/vimtex",
-      ft = "latex",
+      ft = { "tex", "latex" },
       init = function()
          vim.g.vimtex_view_method = "zathura"
          vim.g.vimtex_compiler_latexmk = {
